4c0e60a85883806eac8f8d6df263a3ec85564989
Varun Saketh .R <varunsaketh6167@gmail.com>
2026-03-31 18:35:33 +0530

    Create DataWarehouse database and schemas
    This script creates a new database named 'DataWarehouse' and three schemas: Bronze, Silver, and Gold.

--- scripts/creating_Database_and_Schemas.sql
+++ scripts/creating_Database_and_Schemas.sql
@@ -1,1 +1,12 @@
 
+-- Creates New Database called as Datawarehouse 
+CREATE DATABASE DataWarehouse;
+USE DataWarehouse
+-- creates new schema's called as Bronze , silver and gold 
+CREATE SCHEMA Bronze
+GO
+CREATE SCHEMA Silver;
+GO
+CREATE SCHEMA Gold;
+GO
+
